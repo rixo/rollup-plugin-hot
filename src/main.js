37a7415e3bd580110f7fadee38fd1560ea311c4b
rixo <rixo@rixo.fr>
2019-10-19 22:19:52 +0200

    nicer

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'systemjs/dist/system.js'
 import installSystemHooks from './system-hooks'
 import createWebSocketClient from './client'
 import { setDeps } from './deps-map'
-import { hot, applyUpdate, flush } from './hot-api'
+import { hot, applyUpdate, flush } from './hot'
 
 installSystemHooks({ hot, setDeps })
 
