505150fc4e732485303d09b4fd3fe8dabd06f359
rixo <rixo@rixo.fr>
2019-10-24 03:01:13 +0200

    add sub directory to test case

--- test/src/b.js
+++ test/src/b.js
@@ -3,6 +3,6 @@ import { a as az } from './a'
 const a = 'B:' + az
 
 export default async (...args) => {
-  const { c } = await import('./c')
+  const { c } = await import('./sub/c')
   return `B( ${[a, az, c, ...args].join(' | ')} )`
 }
