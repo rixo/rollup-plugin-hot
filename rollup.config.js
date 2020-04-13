5c0923462bad4e348ee7703aa007a933ac5dc053
rixo <rixo@rixo.fr>
2020-04-13 13:54:23 +0200

    add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/main.js',
@@ -14,6 +15,11 @@ export default {
     }),
     commonjs({
       include: 'lib/constants.js',
+    }),
+    terser({
+      compress: {
+        drop_debugger: false,
+      },
     }),
   ],
 }
